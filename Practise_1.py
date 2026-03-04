b216fe46179f04d0a36e4d4f8e420fc8125f5f70
Danil-prog-coder <danillybchik94@gmail.com>
2026-03-04 12:41:12 +0300

    first task in Practise: fix solution without split for string

--- Practise_1.py
+++ Practise_1.py
@@ -4,17 +4,61 @@ base_string_1 = base_string.replace(" ", "_")
 print(base_string_1)
 
 # task 2
+# base_string = input()
+# list_words = base_string.split()
+# print(list_words, len(list_words))
 base_string = input()
-list_words = base_string.split()
-print(list_words, len(list_words))
+
+words = []
+current_word = ""
+
+for char in base_string:
+    if char != " ":
+        current_word += char
+    else:
+        if current_word:
+            words.append(current_word)
+            current_word = ""
+
+if current_word:
+    words.append(current_word)
+
+reversed_words = []
+
+for word in words:
+    reversed_words.append(word[::-1])
+
+print(*reversed_words, sep=" ")
 
 # task 3
+# base_string = input()
+# base_string_3 = ""
+# list_words =[word[::-1] for word in base_string.split()]
+# for word in list_words:
+#     base_string_3 += word[::-1]
+# print(*list_words, sep=" ")
 base_string = input()
-base_string_3 = ""
-list_words =[word[::-1] for word in base_string.split()]
-for word in list_words:
-    base_string_3 += word[::-1]
-print(*list_words, sep=" ")
+
+words = []
+current_word = ""
+
+for char in base_string:
+    if char != " ":
+        current_word += char
+    else:
+        if current_word:
+            words.append(current_word)
+            current_word = ""
+
+if current_word:
+    words.append(current_word)
+
+reversed_words = []
+
+for word in words:
+    reversed_words.append(word[::-1])
+
+print(*reversed_words, sep=" ")
 
 # task 4
 str1 = input()
